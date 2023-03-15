c479d43ea1a22db24db6c835a1f4d970e3c8a301
RathierS <rathier.sylia@gmail.com>
2023-03-15 15:44:08 +0100

    Correction vitesse et fautes

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ var TxtRotate = function(el, toRotate, period) {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
   
     var that = this;
-    var delta = 300 - Math.random() * 100;
+    var delta = 300 - 0.4 * 400;
   
     if (this.isDeleting) { delta /= 2; }
   
